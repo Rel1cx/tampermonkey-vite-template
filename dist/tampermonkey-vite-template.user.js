2a110c8fc409ac24aaeb50a7784632315e76cc28
Eva1ent <let@ik.me>
2023-03-22 23:27:06 +0800

    Add dist dir

---
+++ dist/tampermonkey-vite-template.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         tampermonkey-vite-template
+// @namespace
+// @version      0.0.1
+// @author
+// @description
+// @icon         https://www.bing.com/favicon.ico
+// @source
+// @supportURL
+// @downloadURL
+// @updateURL
+// @match        https://www.bing.com/*
+// @run-at       document-body
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	console.log("Hello World");
+
+})();
